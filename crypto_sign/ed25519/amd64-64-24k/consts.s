b9913b54f175103ad65ac2d217885507e3d19fe6
Adrián Cancio <UjikTL@gmail.com>
2021-03-31 00:34:16 +0200

    Changed from monero to bitzum

--- crypto_sign/ed25519/amd64-64-24k/consts.s
+++ crypto_sign/ed25519/amd64-64-24k/consts.s
@@ -1,3 +1,9 @@
+#ifdef __APPLE__
+  #define BITZUM_LOCAL_SYMBOL(x) .private_extern x
+#else
+  #define BITZUM_LOCAL_SYMBOL(x) .hidden x
+#endif
+
 .data
 
 .globl crypto_sign_ed25519_amd64_64_121666
@@ -15,6 +21,22 @@
 .globl crypto_sign_ed25519_amd64_64_EC2D2
 .globl crypto_sign_ed25519_amd64_64_EC2D3
 .globl crypto_sign_ed25519_amd64_64_38
+
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_121666)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_MU0)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_MU1)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_MU2)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_MU3)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_MU4)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_ORDER0)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_ORDER1)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_ORDER2)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_ORDER3)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_EC2D0)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_EC2D1)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_EC2D2)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_EC2D3)
+BITZUM_LOCAL_SYMBOL(crypto_sign_ed25519_amd64_64_38)
 
 .p2align 4
 
